be9283b378f0c2a61cd5f4cf2365f57dfd5182d4
Rustem <a1410453@gmail.com>
2022-10-28 22:01:09 +0600

    Initial Commit

---
+++ xando/xandoApp.swift
@@ -0,0 +1,17 @@
+//
+//  xandoApp.swift
+//  xando
+//
+//  Created by Rustem Orazbayev on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct xandoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
